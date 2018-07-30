f5443c112320886ac49be8cf3b7300131b93c342
Jay Chen <webmaster@webther.com>
2018-07-29 20:12:34 -0400

    Added API data.

--- src/api/commerce.js
+++ src/api/commerce.js
@@ -4,7 +4,8 @@
 const _products = [
   { pid: 1, title: "iPad 4 Mini", price: 499.99, inventory: 2 },
   { pid: 2, title: "H&M T-Shirt White", price: 10.99, inventory: 14 },
-  { pid: 3, title: "Charli XCX - Sucker CD", price: 19.99, inventory: 5 }
+  { pid: 3, title: "Charli XCX - Sucker CD", price: 19.99, inventory: 5 },
+  { pid: 4, title: "Google Pixel 3", price: 799.99, inventory: 6 }
 ];
 
 export default {
